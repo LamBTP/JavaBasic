a79797b8a018a9186ddb95c45f5eb1f069d04682
LamBTP <phuonglam0501202@gmail.com>
2025-11-19 00:46:14 +0700

    JAVAOOP

--- JavaBasic/src/VonglapFor/DemoVonglapFor.java
+++
@@ -1,9 +0,0 @@
-package VonglapFor;
-
-public class DemoVonglapFor {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i+=2) {
-            System.out.println("đây là vòng lặp:" + i);
-        }
-    }
-}
